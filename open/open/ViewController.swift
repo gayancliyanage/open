733ecbb6fc711529454cf973665cb1d949e55d96
Server <server@Gayans-MacBook-Pro.local>
2019-03-21 21:51:22 +0800

    init

---
+++ open/open/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  open
+//
+//  Created by Server on 21/3/19.
+//  Copyright © 2019 Server. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
